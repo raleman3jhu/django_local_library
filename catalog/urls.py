5fa2445b68681ea5dd9a6e2a4950b391d2b34c79
raleman3 <raleman3@jhu.edu>
2024-06-27 17:34:20 -0400

    additions

--- catalog/urls.py
+++ catalog/urls.py
@@ -9,5 +9,29 @@ urlpatterns = [
     path('books/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
     path('authors/', views.AuthorListView.as_view(), name='authors'),
     path('author/<int:pk>', views.AuthorDetailView.as_view(), name='author-detail'),
+]
+
+urlpatterns += [
     path('mybooks/', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
+    path(r'borrowed/', views.LoanedBooksAllListView.as_view(), name='all-borrowed'),
+]
+
+
+# Add URLConf for librarian to renew a book.
+urlpatterns += [
+    path('book/<uuid:pk>/renew/', views.renew_book_librarian,
+         name='renew-book-librarian'),
+]
+
+# Add URLConf to list, view, create, update, and delete bookinstances
+urlpatterns += [
+    path('bookinstances/', views.BookInstanceListView.as_view(), name='bookinstances'),
+    path('bookinstance/<uuid:pk>', views.BookInstanceDetailView.as_view(),
+         name='bookinstance-detail'),
+    path('bookinstance/create/', views.BookInstanceCreate.as_view(),
+         name='bookinstance-create'),
+    path('bookinstance/<uuid:pk>/update/',
+         views.BookInstanceUpdate.as_view(), name='bookinstance-update'),
+    path('bookinstance/<uuid:pk>/delete/',
+         views.BookInstanceDelete.as_view(), name='bookinstance-delete'),
 ]
